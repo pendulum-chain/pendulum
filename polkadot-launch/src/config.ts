da3d519b5e530b4e04b2e47259f0d1e31150a199
Justin Cremer <jacremer@live.com>
2022-01-26 23:33:08 -0800

    clean up launch script + remove build directory

--- polkadot-launch/src/config.ts
+++ polkadot-launch/src/config.ts
@@ -1,5 +1,5 @@
 // Collator flags
-const flags = ["--unsafe-ws-external", "--force-authoring", "--", "--execution=wasm"];
+const flags = ["--unsafe-rpc-external", "--unsafe-ws-external", "--rpc-cors=all", "--force-authoring", "--", "--execution=wasm"];
 
 const relay = {
     bin: "../../bin/polkadot",
@@ -7,60 +7,65 @@ const relay = {
     nodes: [
         {
             name: "alice",
-            wsPort: 9944,
-            port: 30444,
+            port: 10000,
+            rpcPort: 20000,
+            wsPort: 30000,
         },
         {
             name: "bob",
-            wsPort: 9955,
-            port: 30555,
+            port: 10001,
+            rpcPort: 20002,
+            wsPort: 30003,
         },
     ],
-    genesis: {
-        runtime: {
-            runtime_genesis_config: {
-                configuration: {
-                    config: {
-                        validation_upgrade_frequency: 10,
-                        validation_upgrade_delay: 10,
-                    },
-                },
+    // genesis: {
+    //     runtime: {
+    //         runtime_genesis_config: {
+    //             configuration: {
+    //                 config: {
+    //                     validation_upgrade_frequency: 10,
+    //                     validation_upgrade_delay: 10,
+    //                 },
+    //             },
+    //         },
+    //     },
+    // },
+};
+
+const collators = {
+    pendulum: {
+        bin: "../../bin/polkadot-collator",
+        id: "200",
+        balance: "1000000",
+        nodes: [
+            {
+                name: "alice",
+                port: 10002,
+                rpcPort: 20002,
+                wsPort: 30002,
+                flags,
             },
-        },
+        ],
     },
-};
-
-const polkadot_collator = {
-    bin: "polkadot-collator",
-    id: "200",
-    balance: "1000000",
-    nodes: [
-        {
-            wsPort: 9988,
-            port: 31200,
-            name: "alice",
-            flags,
-        },
-    ],
-};
-
-const pendulum_collator = {
-    bin: "../../target/release/parachain-collator",
-    id: "300",
-    balance: "1000000000000000000000",
-    nodes: [
-        {
-            wsPort: 9999,
-            port: 31300,
-            name: "alice",
-            flags,
-        },
-    ],
-};
+    cumulus: {
+        bin: "../../target/release/parachain-collator",
+        id: "300",
+        balance: "1000000",
+        nodes: [
+            {
+                name: "alice",
+                port: 10003,
+                rpcPort: 20003,
+                wsPort: 30003,
+                flags,
+            },
+        ],
+    }
+}
 
 export const config = {
     relaychain: relay,
-    parachains: [pendulum_collator],
+    parachains: [collators.pendulum],
     types: {},
     finalization: false,
 };

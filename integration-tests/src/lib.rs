ba5080a3c5b4a12f2242f8587378530f216130a4
web3online <placex.com@gmail.com>
2022-09-19 18:03:49 +0100

    configure kusama runtime. fix ExceedsMaxMessageSize

--- integration-tests/src/lib.rs
+++ integration-tests/src/lib.rs
@@ -6,6 +6,8 @@ pub const INITIAL_BALANCE: u128 = 1_000_000_000;
 use xcm_simulator::TestExternalities;
 use polkadot_parachain::primitives::Id as ParaId;
 use frame_support::traits::GenesisBuild;
+use polkadot_runtime_parachains::configuration::HostConfiguration;
+use polkadot_primitives::v2::{BlockNumber, MAX_CODE_SIZE, MAX_POV_SIZE};
 // use primitives::AccountId;
 
 decl_test_parachain! {
@@ -76,12 +78,11 @@ pub fn kusama_ext() -> TestExternalities {
     .assimilate_storage(&mut t)
     .unwrap();
 
-	//TODO
-    // polkadot_runtime_parachains::configuration::GenesisConfig::<Runtime> {
-    //     config: default_parachains_host_configuration(),
-    // }
-    // .assimilate_storage(&mut t)
-    // .unwrap();
+    polkadot_runtime_parachains::configuration::GenesisConfig::<Runtime> {
+        config: default_parachains_host_configuration(),
+    }
+    .assimilate_storage(&mut t)
+    .unwrap();
 
     <pallet_xcm::GenesisConfig as GenesisBuild<Runtime>>::assimilate_storage(
         &pallet_xcm::GenesisConfig {
@@ -104,6 +105,45 @@ pub fn ksm(n: f64) -> u128 {
 pub fn para_account_id(id: u32) -> sp_runtime::AccountId32 {
 	ParaId::from(id).into_account_truncating()
 }
+
+fn default_parachains_host_configuration() -> HostConfiguration<BlockNumber> {
+    HostConfiguration {
+        validation_upgrade_cooldown: 2u32,
+        validation_upgrade_delay: 2,
+        code_retention_period: 1200,
+        max_code_size: MAX_CODE_SIZE,
+        max_pov_size: MAX_POV_SIZE,
+        max_head_data_size: 32 * 1024,
+        group_rotation_frequency: 20,
+        chain_availability_period: 4,
+        thread_availability_period: 4,
+        max_upward_queue_count: 8,
+        max_upward_queue_size: 1024 * 1024,
+        max_downward_message_size: 1024 * 1024,
+        ump_service_total_weight: 100_000_000_000,
+        max_upward_message_size: 50 * 1024,
+        max_upward_message_num_per_candidate: 5,
+        hrmp_sender_deposit: 0,
+        hrmp_recipient_deposit: 0,
+        hrmp_channel_max_capacity: 8,
+        hrmp_channel_max_total_size: 8 * 1024,
+        hrmp_max_parachain_inbound_channels: 4,
+        hrmp_max_parathread_inbound_channels: 4,
+        hrmp_channel_max_message_size: 1024 * 1024,
+        hrmp_max_parachain_outbound_channels: 4,
+        hrmp_max_parathread_outbound_channels: 4,
+        hrmp_max_message_num_per_candidate: 5,
+        dispute_period: 6,
+        no_show_slots: 2,
+        n_delay_tranches: 25,
+        needed_approvals: 2,
+        relay_vrf_modulo_samples: 2,
+        zeroth_delay_tranche_width: 0,
+        minimum_validation_upgrade_delay: 5,
+        ..Default::default()
+    }
+}
+
 
 pub type RelayChainPalletXcm = pallet_xcm::Pallet<kusama_runtime::Runtime>;
 pub type ParachainPalletXcm = pallet_xcm::Pallet<amplitude_runtime::Runtime>;
@@ -145,7 +185,7 @@ mod tests {
 			use amplitude_runtime::{Event, System};
 			assert!(System::events().iter().any(|r| matches!(
 				r.event,
-				Runtime::System(frame_system::Event::Remarked { .. })
+				Event::System(frame_system::Event::Remarked { .. })
 			)));
 		});
 	}

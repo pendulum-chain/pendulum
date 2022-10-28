705913e12f4f22fd0387b5547442660117b2fe3d
web3online <placex.com@gmail.com>
2022-10-28 15:09:33 +0100

    increment spec_version and transaction_version

--- runtime/amplitude/src/lib.rs
+++ runtime/amplitude/src/lib.rs
@@ -147,10 +147,10 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("amplitude"),
 	impl_name: create_runtime_str!("amplitude"),
 	authoring_version: 1,
-	spec_version: 4,
+	spec_version: 5,
 	impl_version: 0,
 	apis: RUNTIME_API_VERSIONS,
-	transaction_version: 4,
+	transaction_version: 5,
 	state_version: 1,
 };
 

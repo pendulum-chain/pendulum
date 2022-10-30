0cfd1729b150738c4a11abfdcadf3063d4026617
web3online <102041955+web3online@users.noreply.github.com>
2022-10-30 13:24:34 +0000

    Runtime upgrades (#72)
    * increment spec_version and transaction_version
    
    * allow transfer back to relay-chain
    
    * update runtime version

--- runtime/amplitude/src/lib.rs
+++ runtime/amplitude/src/lib.rs
@@ -147,10 +147,10 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("amplitude"),
 	impl_name: create_runtime_str!("amplitude"),
 	authoring_version: 1,
-	spec_version: 5,
+	spec_version: 6,
 	impl_version: 0,
 	apis: RUNTIME_API_VERSIONS,
-	transaction_version: 5,
+	transaction_version: 6,
 	state_version: 1,
 };
 

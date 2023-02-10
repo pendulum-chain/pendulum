d382fec7ee9ca4641a9186ef1c121e91f08f4b81
b-yap <2826165+b-yap@users.noreply.github.com>
2023-02-11 01:48:09 +0800

    one more nonzero in signed type

--- runtime/foucoco/src/lib.rs
+++ runtime/foucoco/src/lib.rs
@@ -116,6 +116,7 @@ pub type BlockId = generic::BlockId<Block>;
 
 /// The SignedExtension to the basic transaction logic.
 pub type SignedExtra = (
+	frame_system::CheckNonZeroSender<Runtime>,
 	frame_system::CheckSpecVersion<Runtime>,
 	frame_system::CheckTxVersion<Runtime>,
 	frame_system::CheckGenesis<Runtime>,
@@ -319,16 +320,16 @@ impl Contains<RuntimeCall> for BaseFilter {
 			RuntimeCall::Contracts(_) |
 			RuntimeCall::ZenlinkProtocol(_) |
 			RuntimeCall::DiaOracleModule(_) |
-			RuntimeCall::Security(_) |
-			RuntimeCall::Oracle(_) |
-			RuntimeCall::StellarRelay(_) |
-			RuntimeCall::VaultRewards(_) |
 			RuntimeCall::Fee(_) |
-			RuntimeCall::VaultRegistry(_) |
-			RuntimeCall::Redeem(_) |
 			RuntimeCall::Issue(_) |
 			RuntimeCall::Nomination(_) |
-			RuntimeCall::Replace(_) => true,
+			RuntimeCall::Oracle(_) |
+			RuntimeCall::Redeem(_) |
+			RuntimeCall::Replace(_) |
+			RuntimeCall::Security(_) |
+			RuntimeCall::StellarRelay(_) |
+			RuntimeCall::VaultRegistry(_) |
+			RuntimeCall::VaultRewards(_) => true,
 			// All pallets are allowed, but exhaustive match is defensive
 			// in the case of adding new pallets.
 		}
@@ -975,6 +976,7 @@ where
 		let current_block = System::block_number().saturated_into::<u64>().saturating_sub(1);
 		let tip = 0;
 		let extra: SignedExtra = (
+			frame_system::CheckNonZeroSender::<Runtime>::new(),
 			frame_system::CheckSpecVersion::<Runtime>::new(),
 			frame_system::CheckTxVersion::<Runtime>::new(),
 			frame_system::CheckGenesis::<Runtime>::new(),

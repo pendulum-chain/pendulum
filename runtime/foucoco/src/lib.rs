9bad2eb6b67450d0eadbaec1c8c3db6553798ed2
Hugo Lindsay <hugo@satoshipay.io>
2023-03-16 10:14:36 -0400

    add missing dia chain ext

--- runtime/foucoco/src/lib.rs
+++ runtime/foucoco/src/lib.rs
@@ -1226,10 +1226,21 @@ where
 					.map_err(|_| DispatchError::Other("ChainExtension failed to call balance"))?;
 			},
 
-			//TODO
+			//dia price feed
 			7777 => {
-				error!("Called an dia oracle `func_id`: {:}", func_id);
-				return Err(DispatchError::Other("Unimplemented dia oracle func_id"))
+				let mut env = env.buf_in_buf_out();
+				let price_feed_request: ([u8; 32], [u8; 32]) =
+					env.read_as()?;
+
+				let blockchain = price_feed_request.0; 
+				let symbol = price_feed_request.1;
+				let price_feed = <dia_oracle::Pallet::<T> as DiaOracle>::get_coin_info(blockchain.to_vec(), symbol.to_vec());
+
+				warn!("price_feed_request : {:#?}", price_feed_request);
+				warn!("price_feed : {:#?}", price_feed);
+
+				env.write(&price_feed.encode(), false, None)
+					.map_err(|_| DispatchError::Other("ChainExtension failed to call price feed"))?;
 			},
 			_ => {
 				error!("Called an unregistered `func_id`: {:}", func_id);

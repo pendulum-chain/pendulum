84d9f505ae617c5666002723f6cb6d0804d3703b
cr4pt0 <placex.com@gmail.com>
2023-03-20 16:36:39 +0000

    add test cases to add and remove multiple currency id via extrinsic

--- pallets/orml-currencies-allowance-extension/src/tests.rs
+++ pallets/orml-currencies-allowance-extension/src/tests.rs
@@ -52,3 +52,41 @@ fn should_not_remove_allowed_currencies_with_not_root_origin() {
 		assert_err!(result, BadOrigin);
 	})
 }
+
+#[test]
+fn should_add_few_allowed_currencies() {
+	run_test(|| {
+		let native_currency_id = <Test as orml_currencies::Config>::GetNativeCurrencyId::get();
+		let added_currencies: Vec<CurrencyOf<Test>> = vec![native_currency_id, 1, 2, 3];
+		assert_ok!(TokenAllowance::add_allowed_currencies(RuntimeOrigin::root(), added_currencies));
+		assert_eq!(AllowedCurrencies::<Test>::get(native_currency_id), Some(()));
+		assert_eq!(AllowedCurrencies::<Test>::get(1), Some(()));
+		assert_eq!(AllowedCurrencies::<Test>::get(2), Some(()));
+		assert_eq!(AllowedCurrencies::<Test>::get(3), Some(()));
+	})
+}
+
+#[test]
+fn should_remove_few_allowed_currencies() {
+	run_test(|| {
+		let native_currency_id = <Test as orml_currencies::Config>::GetNativeCurrencyId::get();
+		let added_currencies: Vec<CurrencyOf<Test>> = vec![native_currency_id, 1, 2, 3];
+		assert_ok!(TokenAllowance::add_allowed_currencies(
+			RuntimeOrigin::root(),
+			added_currencies.clone()
+		));
+		assert_eq!(AllowedCurrencies::<Test>::get(native_currency_id), Some(()));
+		assert_eq!(AllowedCurrencies::<Test>::get(1), Some(()));
+		assert_eq!(AllowedCurrencies::<Test>::get(2), Some(()));
+		assert_eq!(AllowedCurrencies::<Test>::get(3), Some(()));
+
+		assert_ok!(TokenAllowance::remove_allowed_currencies(
+			RuntimeOrigin::root(),
+			added_currencies
+		));
+		assert_eq!(AllowedCurrencies::<Test>::get(native_currency_id), None);
+		assert_eq!(AllowedCurrencies::<Test>::get(1), None);
+		assert_eq!(AllowedCurrencies::<Test>::get(2), None);
+		assert_eq!(AllowedCurrencies::<Test>::get(3), None);
+	})
+}

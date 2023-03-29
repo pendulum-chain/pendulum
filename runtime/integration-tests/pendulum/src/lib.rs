2717818d5563900c40c16d9493e23caa86883acf
cr4pt0 <placex.com@gmail.com>
2023-03-29 21:48:29 +0100

    add xcm test configuration

--- runtime/integration-tests/pendulum/src/lib.rs
+++ runtime/integration-tests/pendulum/src/lib.rs
@@ -1,2 +1,134 @@
+use frame_support::{
+	construct_runtime, parameter_types,
+	traits::{Everything, Nothing},
+};
+use sp_core::H256;
+use sp_runtime::{testing::Header, traits::IdentityLookup, AccountId32};
+
+use polkadot_parachain::primitives::Id as ParaId;
+use polkadot_runtime_parachains::{configuration, origin, shared, ump};
+use xcm::latest::prelude::*;
+use xcm_builder::{
+	AccountId32Aliases, AllowUnpaidExecutionFrom, ChildParachainAsNative,
+	ChildParachainConvertsVia, ChildSystemParachainAsSuperuser,
+	CurrencyAdapter as XcmCurrencyAdapter, FixedRateOfFungible, FixedWeightBounds, IsConcrete,
+	LocationInverter, SignedAccountId32AsNative, SignedToAccountId32, SovereignSignedViaLocation,
+};
+use xcm_executor::{Config, XcmExecutor};
+
+pub type AccountId = AccountId32;
+pub type Balance = u128;
+
+use polkadot_core_primitives::BlockNumber;
+use polkadot_primitives::v2::{MAX_CODE_SIZE, MAX_POV_SIZE};
+use polkadot_runtime_parachains::configuration::HostConfiguration;
 use sp_runtime::traits::AccountIdConversion;
 use xcm_simulator::{decl_test_network, decl_test_parachain, decl_test_relay_chain};
+use frame_support::traits::GenesisBuild;
+use frame_support::weights::Weight;
+use polkadot_parachain::primitives::{
+	DmpMessageHandler, Sibling, XcmpMessageFormat, XcmpMessageHandler,
+};
+
+
+decl_test_relay_chain! {
+	pub struct Relay {
+		Runtime = polkadot_runtime::Runtime,
+		XcmConfig = polkadot_runtime::xcm_config::XCMConfig,
+		new_ext = relay_ext(),
+	}
+}
+
+decl_test_network! {
+    pub struct TestNet{
+        relay_chain = Relay,
+        parachains = vec![ ]
+    }
+}
+
+
+pub fn relay_ext() -> sp_io::TestExternalities {
+    use polkadot_runtime::{Runtime, System};
+    let mut t = frame_system::GenesisConfig::default()
+        .build_storage::<Runtime>()
+        .unwrap();
+    pallet_balances::GenesisConfig::<Runtime> {
+        balances: vec![
+            // (AccountId::from(ALICE), cfg(2002)),
+            // (
+            //     ParaId::from(parachains::polkadot::centrifuge::ID).into_account_truncating(),
+            //     cfg(7),
+            // ),
+            // (
+            //     ParaId::from(PARA_ID_SIBLING).into_account_truncating(),
+            //     cfg(7),
+            // ),
+        ],
+    }
+    .assimilate_storage(&mut t)
+    .unwrap();
+    polkadot_runtime_parachains::configuration::GenesisConfig::<Runtime> {
+        config: default_parachains_host_configuration(),
+    }
+    .assimilate_storage(&mut t)
+    .unwrap();
+    <pallet_xcm::GenesisConfig as GenesisBuild<Runtime>>::assimilate_storage(
+        &pallet_xcm::GenesisConfig {
+            safe_xcm_version: Some(2),
+        },
+        &mut t,
+    )
+    .unwrap();
+    let mut ext = sp_io::TestExternalities::new(t);
+    ext.execute_with(|| System::set_block_number(1));
+    ext
+}
+
+fn default_parachains_host_configuration() -> HostConfiguration<BlockNumber> {
+    HostConfiguration {
+        minimum_validation_upgrade_delay: 5,
+        validation_upgrade_cooldown: 5u32,
+        validation_upgrade_delay: 5,
+        code_retention_period: 1200,
+        max_code_size: MAX_CODE_SIZE,
+        max_pov_size: MAX_POV_SIZE,
+        max_head_data_size: 32 * 1024,
+        group_rotation_frequency: 20,
+        chain_availability_period: 4,
+        thread_availability_period: 4,
+        max_upward_queue_count: 8,
+        max_upward_queue_size: 1024 * 1024,
+        max_downward_message_size: 1024,
+        ump_service_total_weight: Weight::from_ref_time(4 * 1_000_000_000),
+        max_upward_message_size: 50 * 1024,
+        max_upward_message_num_per_candidate: 5,
+        hrmp_sender_deposit: 0,
+        hrmp_recipient_deposit: 0,
+        hrmp_channel_max_capacity: 8,
+        hrmp_channel_max_total_size: 8 * 1024,
+        hrmp_max_parachain_inbound_channels: 4,
+        hrmp_max_parathread_inbound_channels: 4,
+        hrmp_channel_max_message_size: 1024 * 1024,
+        hrmp_max_parachain_outbound_channels: 4,
+        hrmp_max_parathread_outbound_channels: 4,
+        hrmp_max_message_num_per_candidate: 5,
+        dispute_period: 6,
+        no_show_slots: 2,
+        n_delay_tranches: 25,
+        needed_approvals: 2,
+        relay_vrf_modulo_samples: 2,
+        zeroth_delay_tranche_width: 0,
+        ..Default::default()
+    }
+}
+
+
+#[cfg(test)]
+mod tests {
+	use super::*;
+
+    #[test]
+	fn dmp() {
+
+    }
+}

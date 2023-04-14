7518d6178a873e44cd3ef9a7062e61817968ddb5
Rust Ninja <102041955+RustNinja@users.noreply.github.com>
2023-04-14 18:30:59 +0100

    bump foucoco spec version to perform runtime upgrade (#212)

--- runtime/foucoco/src/lib.rs
+++ runtime/foucoco/src/lib.rs
@@ -244,10 +244,10 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("amplitude"),
 	impl_name: create_runtime_str!("amplitude"),
 	authoring_version: 1,
-	spec_version: 9,
+	spec_version: 10,
 	impl_version: 0,
 	apis: RUNTIME_API_VERSIONS,
-	transaction_version: 20,
+	transaction_version: 21,
 	state_version: 1,
 };
 

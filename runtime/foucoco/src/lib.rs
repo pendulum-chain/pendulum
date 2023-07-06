7100012ceb29b2a487fbe7416556d8f60c4b1473
b-yap <2826165+b-yap@users.noreply.github.com>
2023-07-06 16:03:58 +0800

    revert version

--- runtime/foucoco/src/lib.rs
+++ runtime/foucoco/src/lib.rs
@@ -246,7 +246,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("amplitude"),
 	impl_name: create_runtime_str!("amplitude"),
 	authoring_version: 1,
-	spec_version: 15,
+	spec_version: 14,
 	impl_version: 0,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 25,

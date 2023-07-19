c113f89cb79144ba0a299d5fb211aaac00470227
adelarja <adelarja@gmail.com>
2023-07-19 13:39:47 -0300

    Renamed spec_name and impl_name from 'amplitude' to 'foucoco'

--- runtime/foucoco/src/lib.rs
+++ runtime/foucoco/src/lib.rs
@@ -243,8 +243,8 @@ impl_opaque_keys! {
 
 #[sp_version::runtime_version]
 pub const VERSION: RuntimeVersion = RuntimeVersion {
-	spec_name: create_runtime_str!("amplitude"),
-	impl_name: create_runtime_str!("amplitude"),
+	spec_name: create_runtime_str!("foucoco"),
+	impl_name: create_runtime_str!("foucoco"),
 	authoring_version: 1,
 	spec_version: 1,
 	impl_version: 0,

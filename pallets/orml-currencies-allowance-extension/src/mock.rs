564f4c0340e2f43d7a0951546c52f125044e2b23
Marcel Ebert <mail@marcel-ebert.de>
2023-07-24 13:45:40 +0200

    Fix wrong config definition in mock

--- pallets/orml-currencies-allowance-extension/src/mock.rs
+++ pallets/orml-currencies-allowance-extension/src/mock.rs
@@ -136,7 +136,7 @@ impl orml_currencies::Config for Test {
 
 impl Config for Test {
 	type RuntimeEvent = RuntimeEvent;
-	type WeightInfo = token_allowance::default_weights::WeightInfo<Test>;
+	type WeightInfo = token_allowance::default_weights::SubstrateWeight<Test>;
 	type MaxAllowedCurrencies = ConstU32<256>;
 }
 

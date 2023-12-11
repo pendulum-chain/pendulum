04a4d27d3ba91ccd7ee865c260b5f005ab0adb5e
gianfra-t <96739519+gianfra-t@users.noreply.github.com>
2023-12-11 10:47:36 -0300

    333 extend the orml tokens pallet so users are able to mint and burn custom assets (#359)
    * initial code for extension pallet
    
    * very basic implementation
    
    * format
    
    * add benchmarks
    
    * add benchmarked weights, mock data feeder
    
    * use MockDataFeeder directly from spacewalk
    
    * add force transfer ownership by root, tests
    
    * rename orml token extension to orml-tokens-management-extension
    
    * comments fixes, remove 'Destroyed' event
    
    * add deposit mechanics
    
    * Fix wrong crate references
    
    * Remove `GetTestCurrency` config parameter
    
    * Fix benchmarks
    
    * rollback incorrect mock modification
    
    * re-run benchmarks to account for deposit
    
    ---------
    
    Co-authored-by: Marcel Ebert <mail@marcel-ebert.de>

---
+++ pallets/orml-tokens-management-extension/src/tests.rs
@@ -0,0 +1,288 @@
+use crate::{
+	mock::*,
+	types::{AccountIdOf, CurrencyDetails, CurrencyOf},
+	Config, Error,
+};
+use frame_support::{assert_err, assert_ok, traits::Get};
+use orml_traits::{MultiCurrency, MultiReservableCurrency};
+
+fn get_balance(currency_id: CurrencyOf<Test>, account: &AccountId) -> Balance {
+	<orml_currencies::Pallet<Test> as MultiCurrency<AccountId>>::free_balance(currency_id, account)
+}
+
+fn get_reserved_balance(currency_id: CurrencyOf<Test>, account: &AccountId) -> Balance {
+	<orml_currencies::Pallet<Test> as MultiReservableCurrency<AccountId>>::reserved_balance(
+		currency_id,
+		account,
+	)
+}
+
+fn get_total_issuance(currency_id: CurrencyOf<Test>) -> Balance {
+	<orml_currencies::Pallet<Test> as MultiCurrency<AccountId>>::total_issuance(currency_id)
+}
+
+#[test]
+fn can_create_currency_and_mint() {
+	run_test(|| {
+		let amount_minted = 10;
+		let owner_id = USER_0;
+		let beneficiary_id = USER_1;
+		let currency_id: CurrencyOf<Test> = 1;
+		let deposit = <Test as Config>::AssetDeposit::get();
+
+		assert_ok!(crate::Pallet::<Test>::create(RuntimeOrigin::signed(owner_id), currency_id,));
+
+		assert_eq!(
+			get_reserved_balance(<Test as Config>::DepositCurrency::get(), &owner_id),
+			deposit
+		);
+
+		assert_ok!(crate::Pallet::<Test>::mint(
+			RuntimeOrigin::signed(owner_id),
+			currency_id,
+			beneficiary_id,
+			amount_minted
+		));
+
+		assert_eq!(get_balance(currency_id, &beneficiary_id), amount_minted);
+		assert_eq!(get_total_issuance(currency_id), amount_minted);
+	})
+}
+
+#[test]
+fn cannot_create_if_not_enough_balance_for_deposit() {
+	run_test(|| {
+		let owner_id = USER_3;
+		let currency_id: CurrencyOf<Test> = 1;
+
+		assert_err!(
+			crate::Pallet::<Test>::create(RuntimeOrigin::signed(owner_id), currency_id,),
+			Error::<Test>::InsufficientBalance
+		);
+	})
+}
+
+#[test]
+fn cannot_mint_if_not_owner() {
+	run_test(|| {
+		let amount_minted = 10;
+		let currency_id: CurrencyOf<Test> = 1;
+		let owner_id = USER_0;
+		let beneficiary_id = USER_1;
+		let not_owner_id = USER_2;
+
+		assert_ok!(crate::Pallet::<Test>::create(RuntimeOrigin::signed(owner_id), currency_id,));
+
+		assert_err!(
+			crate::Pallet::<Test>::mint(
+				RuntimeOrigin::signed(not_owner_id),
+				currency_id,
+				beneficiary_id,
+				amount_minted
+			),
+			Error::<Test>::NoPermission
+		);
+	})
+}
+
+#[test]
+fn cannot_create_invalid_currency() {
+	run_test(|| {
+		let owner_id = USER_0;
+		// We only allow 0-9, so we use 10 to test
+		let invalid_currency: CurrencyOf<Test> = 10;
+
+		assert_err!(
+			crate::Pallet::<Test>::create(RuntimeOrigin::signed(owner_id), invalid_currency,),
+			Error::<Test>::NotOwnableCurrency
+		);
+	})
+}
+
+#[test]
+fn can_mint_and_burn() {
+	run_test(|| {
+		let amount_minted = 10;
+		let amount_burned = 5;
+		let owner_id = USER_0;
+		let beneficiary_id = USER_1;
+		let currency: CurrencyOf<Test> = 1;
+
+		assert_ok!(crate::Pallet::<Test>::create(RuntimeOrigin::signed(owner_id), currency));
+
+		assert_ok!(crate::Pallet::<Test>::mint(
+			RuntimeOrigin::signed(owner_id),
+			currency,
+			beneficiary_id,
+			amount_minted
+		));
+
+		assert_ok!(crate::Pallet::<Test>::burn(
+			RuntimeOrigin::signed(owner_id),
+			currency,
+			beneficiary_id,
+			amount_burned
+		));
+
+		assert_eq!(get_balance(currency, &beneficiary_id), (amount_minted - amount_burned));
+		assert_eq!(get_total_issuance(currency), (amount_minted - amount_burned));
+	})
+}
+
+#[test]
+fn can_change_ownership() {
+	run_test(|| {
+		let creator_id = USER_0;
+		let new_owner_id = USER_1;
+		let currency_id: CurrencyOf<Test> = 1;
+
+		let deposit = <Test as Config>::AssetDeposit::get();
+		assert_ok!(crate::Pallet::<Test>::create(RuntimeOrigin::signed(creator_id), currency_id,));
+
+		let reserved_balance_owner_before =
+			get_reserved_balance(<Test as Config>::DepositCurrency::get(), &creator_id);
+		let reserved_balance_new_owner_before =
+			get_reserved_balance(<Test as Config>::DepositCurrency::get(), &new_owner_id);
+
+		assert_ok!(crate::Pallet::<Test>::transfer_ownership(
+			RuntimeOrigin::signed(creator_id),
+			currency_id,
+			new_owner_id
+		));
+
+		assert_eq!(
+			get_reserved_balance(<Test as Config>::DepositCurrency::get(), &creator_id),
+			(reserved_balance_owner_before - deposit)
+		);
+		assert_eq!(
+			get_reserved_balance(<Test as Config>::DepositCurrency::get(), &new_owner_id),
+			(reserved_balance_new_owner_before + deposit)
+		);
+
+		assert_eq!(
+			crate::Pallet::<Test>::currency_details(currency_id),
+			Some(CurrencyDetails::<AccountIdOf<Test>, Balance> {
+				owner: new_owner_id,
+				issuer: creator_id,
+				admin: creator_id,
+				deposit
+			})
+		);
+	})
+}
+
+#[test]
+fn cannot_change_ownership_if_not_owner() {
+	run_test(|| {
+		let creator_id = USER_0;
+		let new_owner_id = USER_1;
+		let fake_creator_id = USER_2;
+		let currency_id: CurrencyOf<Test> = 1;
+
+		assert_ok!(crate::Pallet::<Test>::create(RuntimeOrigin::signed(creator_id), currency_id,));
+
+		assert_err!(
+			crate::Pallet::<Test>::transfer_ownership(
+				RuntimeOrigin::signed(fake_creator_id),
+				currency_id,
+				new_owner_id
+			),
+			Error::<Test>::NoPermission
+		);
+	})
+}
+
+#[test]
+fn root_can_change_ownership() {
+	run_test(|| {
+		let creator_id = USER_0;
+		let new_owner_id = USER_1;
+		let deposit = <Test as Config>::AssetDeposit::get();
+		let currency_id: CurrencyOf<Test> = 1;
+
+		assert_ok!(crate::Pallet::<Test>::create(RuntimeOrigin::signed(creator_id), currency_id,));
+
+		let reserved_balance_owner_before =
+			get_reserved_balance(<Test as Config>::DepositCurrency::get(), &creator_id);
+		let reserved_balance_new_owner_before =
+			get_reserved_balance(<Test as Config>::DepositCurrency::get(), &new_owner_id);
+
+		assert_ok!(crate::Pallet::<Test>::force_transfer_ownership(
+			RuntimeOrigin::root(),
+			currency_id,
+			new_owner_id
+		));
+
+		assert_eq!(
+			get_reserved_balance(<Test as Config>::DepositCurrency::get(), &creator_id),
+			(reserved_balance_owner_before - deposit)
+		);
+		assert_eq!(
+			get_reserved_balance(<Test as Config>::DepositCurrency::get(), &new_owner_id),
+			(reserved_balance_new_owner_before + deposit)
+		);
+
+		assert_eq!(
+			crate::Pallet::<Test>::currency_details(currency_id),
+			Some(CurrencyDetails::<AccountIdOf<Test>, Balance> {
+				owner: new_owner_id,
+				issuer: creator_id,
+				admin: creator_id,
+				deposit
+			})
+		);
+	})
+}
+
+#[test]
+fn owner_can_set_managers() {
+	run_test(|| {
+		let creator_id = USER_0;
+		let new_admin = USER_1;
+		let new_issuer = USER_2;
+		let deposit = <Test as Config>::AssetDeposit::get();
+		let currency_id: CurrencyOf<Test> = 1;
+
+		assert_ok!(crate::Pallet::<Test>::create(RuntimeOrigin::signed(creator_id), currency_id,));
+
+		assert_ok!(crate::Pallet::<Test>::set_managers(
+			RuntimeOrigin::signed(creator_id),
+			currency_id,
+			new_admin,
+			new_issuer
+		));
+
+		assert_eq!(
+			crate::Pallet::<Test>::currency_details(currency_id),
+			Some(CurrencyDetails::<AccountIdOf<Test>, Balance> {
+				owner: creator_id,
+				issuer: new_issuer,
+				admin: new_admin,
+				deposit
+			})
+		);
+	})
+}
+
+#[test]
+fn cannot_set_managers_if_not_owner() {
+	run_test(|| {
+		let creator_id = 0;
+		let other_id = 1;
+		let new_admin = 10;
+		let new_issuer = 10;
+		let currency_id: CurrencyOf<Test> = 1;
+
+		assert_ok!(crate::Pallet::<Test>::create(RuntimeOrigin::signed(creator_id), currency_id,));
+
+		assert_err!(
+			crate::Pallet::<Test>::set_managers(
+				RuntimeOrigin::signed(other_id),
+				currency_id,
+				new_admin,
+				new_issuer
+			),
+			Error::<Test>::NoPermission
+		);
+	})
+}

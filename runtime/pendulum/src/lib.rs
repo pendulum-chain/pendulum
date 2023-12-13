7f9a975048769d1ac04cce7140c966d3d03e79f8
Torsten Stüber <15174476+TorstenStueber@users.noreply.github.com>
2023-12-13 14:59:18 -0300

    Bump Pendulum version to 10

--- runtime/pendulum/src/lib.rs
+++ runtime/pendulum/src/lib.rs
@@ -167,10 +167,10 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("pendulum"),
 	impl_name: create_runtime_str!("pendulum"),
 	authoring_version: 1,
-	spec_version: 9,
+	spec_version: 10,
 	impl_version: 0,
 	apis: RUNTIME_API_VERSIONS,
-	transaction_version: 9,
+	transaction_version: 10,
 	state_version: 1,
 };
 

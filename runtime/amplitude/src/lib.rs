41db5b1fd048f263818e16682529c1f96567aa89
Torsten Stüber <15174476+TorstenStueber@users.noreply.github.com>
2024-01-03 09:40:34 +0100

    Bump Amplitude version to 13

--- runtime/amplitude/src/lib.rs
+++ runtime/amplitude/src/lib.rs
@@ -246,10 +246,10 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("amplitude"),
 	impl_name: create_runtime_str!("amplitude"),
 	authoring_version: 1,
-	spec_version: 12,
+	spec_version: 13,
 	impl_version: 0,
 	apis: RUNTIME_API_VERSIONS,
-	transaction_version: 12,
+	transaction_version: 13,
 	state_version: 1,
 };
 

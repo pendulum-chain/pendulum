a3a795b411017096ebdf8047567773a919ae39c7
Marcel Ebert <mail@marcel-ebert.de>
2024-01-11 11:58:39 +0100

    Use proper return values for vesting migration

--- runtime/amplitude/src/lib.rs
+++ runtime/amplitude/src/lib.rs
@@ -158,22 +158,18 @@ pub struct VestingMigrateToV1;
 impl frame_support::traits::OnRuntimeUpgrade for VestingMigrateToV1 {
 	#[cfg(feature = "try-runtime")]
 	fn pre_upgrade() -> Result<Vec<u8>, &'static str> {
-		pallet_vesting::migrations::v1::pre_migrate::<Runtime>();
+		pallet_vesting::migrations::v1::pre_migrate::<Runtime>()?;
 
 		Ok(Vec::new())
 	}
 
 	fn on_runtime_upgrade() -> Weight {
-		pallet_vesting::migrations::v1::migrate::<Runtime>();
-
-		frame_support::weights::Weight::zero()
+		pallet_vesting::migrations::v1::migrate::<Runtime>()
 	}
 
 	#[cfg(feature = "try-runtime")]
 	fn post_upgrade(_state: Vec<u8>) -> Result<(), &'static str> {
-		pallet_vesting::migrations::v1::post_migrate::<Runtime>();
-
-		Ok(())
+		pallet_vesting::migrations::v1::post_migrate::<Runtime>()
 	}
 }
 

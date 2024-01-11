dd83e1b405b6a9b0a7311034f5267d2bf3d2c0a6
bogdanS98 <bogdan.sandu13@gmail.com>
2024-01-12 00:49:49 +0200

    Fmt

--- pallets/treasury-buyout-extension/src/types.rs
+++ pallets/treasury-buyout-extension/src/types.rs
@@ -1,16 +1,14 @@
-use codec::{Encode, Decode, MaxEncodedLen};
+use codec::{Decode, Encode, MaxEncodedLen};
 // use orml_traits::MultiCurrency;
 use scale_info::TypeInfo;
 
 /// Type of amount
-#[derive(
-    Copy, Clone, Debug, Encode, Decode, PartialEq, Eq, TypeInfo, MaxEncodedLen,
-)]
+#[derive(Copy, Clone, Debug, Encode, Decode, PartialEq, Eq, TypeInfo, MaxEncodedLen)]
 pub(crate) enum Amount<Balance> {
-    /// Amount of native asset user get for buyout
-    Buyout(Balance),
-    /// Amount of exchange asset user give for buyout
-    Exchange(Balance),
+	/// Amount of native asset user get for buyout
+	Buyout(Balance),
+	/// Amount of exchange asset user give for buyout
+	Exchange(Balance),
 }
 
 pub(crate) const BUYOUT_LIMIT_PERIOD_IN_SEC: u64 = 86400; // 1 day

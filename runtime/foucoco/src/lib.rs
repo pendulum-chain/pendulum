cbe46145223c9c660304ebc1ea2306d27fe45501
Bogdan Sandu <bogdan.sandu13@gmail.com>
2024-01-19 12:23:41 +0200

    Bump spec and transaction version for Foucoco (#370)
    * Bump spec and transaction version for Foucoco
    
    * Bump spec and transaction version to 7

--- runtime/foucoco/src/lib.rs
+++ runtime/foucoco/src/lib.rs
@@ -256,10 +256,10 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("foucoco"),
 	impl_name: create_runtime_str!("foucoco"),
 	authoring_version: 1,
-	spec_version: 4,
+	spec_version: 7,
 	impl_version: 0,
 	apis: RUNTIME_API_VERSIONS,
-	transaction_version: 4,
+	transaction_version: 7,
 	state_version: 1,
 };
 

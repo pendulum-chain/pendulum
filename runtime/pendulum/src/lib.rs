dcc6bea4870b6ce2a7c54eea1a96f636e95c9fd3
Gianfranco <g.tasteri@gmail.com>
2024-01-26 13:55:13 -0300

    bump spec version

--- runtime/pendulum/src/lib.rs
+++ runtime/pendulum/src/lib.rs
@@ -246,7 +246,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("pendulum"),
 	impl_name: create_runtime_str!("pendulum"),
 	authoring_version: 1,
-	spec_version: 11,
+	spec_version: 12,
 	impl_version: 0,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 10,

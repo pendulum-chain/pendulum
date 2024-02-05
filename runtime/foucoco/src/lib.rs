510b4ffabc112bc4d2ae5854f02c3025f8c71f88
bogdanS98 <bogdan.sandu13@gmail.com>
2024-02-05 13:47:36 +0200

    Fix compiler warning

--- runtime/foucoco/src/lib.rs
+++ runtime/foucoco/src/lib.rs
@@ -1033,7 +1033,7 @@ impl treasury_buyout_extension::PriceGetter<CurrencyId> for OraclePriceGetter {
 		let rate = FixedU128::checked_from_rational(100, 1).expect("This is a valid ratio");
 		// Needed for feeding values to the oracle but is never used in the oracle
 		let account = AccountId::from([0u8; 32]);
-		Oracle::feed_values(account, vec![(key.clone(), rate)]);
+		Oracle::feed_values(account, vec![(key.clone(), rate)])?;
 
 		let asset_price = Oracle::get_price(key.clone()).unwrap_or(FixedU128::one());
 

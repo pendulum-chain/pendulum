7d9dbbc7b650960f611132d333ded24c89d411f4
Torsten Stüber <15174476+TorstenStueber@users.noreply.github.com>
2024-02-25 18:01:20 -0300

    Use our hotfixed version of pallet-contracts

--- runtime/foucoco/src/lib.rs
+++ runtime/foucoco/src/lib.rs
@@ -284,7 +284,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("foucoco"),
 	impl_name: create_runtime_str!("foucoco"),
 	authoring_version: 1,
-	spec_version: 10,
+	spec_version: 12,
 	impl_version: 0,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 8,

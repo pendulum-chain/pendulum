e150990431048a610db2abc37be8e578842b988f
gianfra-t <96739519+gianfra-t@users.noreply.github.com>
2024-03-20 13:55:56 -0300

    bump foucoco spec version (#436)

--- runtime/foucoco/src/lib.rs
+++ runtime/foucoco/src/lib.rs
@@ -316,7 +316,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("foucoco"),
 	impl_name: create_runtime_str!("foucoco"),
 	authoring_version: 1,
-	spec_version: 10,
+	spec_version: 13,
 	impl_version: 0,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 8,

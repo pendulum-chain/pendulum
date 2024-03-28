64528d05e982c81104e28c9d494b3e14e15b4031
Gianfranco <g.tasteri@gmail.com>
2024-03-28 11:33:47 -0300

    move asset hub definition to each parachain

--- runtime/amplitude/src/definitions.rs
+++ runtime/amplitude/src/definitions.rs
@@ -1,5 +1,11 @@
 
-// We reuse the common constant definition for the asset hub
 pub mod asset_hub {
-    pub use runtime_common::parachains::asset_hub::*;
+    use runtime_common::parachain_asset_location;
+
+    pub const PARA_ID: u32 = 1000;
+    pub const ASSET_PALLET_INDEX: u8 = 50;
+
+    parachain_asset_location!(USDC, 1337);
+    parachain_asset_location!(USDT, 1984);
+    parachain_asset_location!(PINK, 23);
 }
--- runtime/common/src/lib.rs
+++ runtime/common/src/lib.rs
@@ -160,18 +160,6 @@ pub mod parachains {
 			}
 		};
 	}
-
-	// These values are shared accross both Pendulum and Kusama
-	pub mod asset_hub {
-		/// values of kusama asset_hub is similar to polkadot's asset_hub
-		pub const PARA_ID: u32 = 1000;
-		pub const ASSET_PALLET_INDEX: u8 = 50;
-
-		parachain_asset_location!(USDC, 1337);
-		parachain_asset_location!(USDT, 1984);
-		parachain_asset_location!(PINK, 23);
-	}
-	
 }
 
 #[cfg(test)]

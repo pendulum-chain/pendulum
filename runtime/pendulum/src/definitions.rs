35b497a24277a026ef99a9ed54d9eb0368c9e97b
Gianfranco <g.tasteri@gmail.com>
2024-04-09 14:20:04 -0300

    remove all unused definitions

--- runtime/pendulum/src/definitions.rs
+++ runtime/pendulum/src/definitions.rs
@@ -5,21 +5,9 @@ pub mod asset_hub {
     pub const PARA_ID: u32 = 1000;
     pub const ASSET_PALLET_INDEX: u8 = 50;
 
-    parachain_asset_location!(USDC, 1337);
-    parachain_asset_location!(USDT, 1984);
-    parachain_asset_location!(PINK, 23);
 }
 
 
-pub mod equilibrium {
-
-    use runtime_common::parachain_asset_location;
-    pub const PARA_ID: u32 = 2011;
-    pub const ASSET_PALLET_INDEX: u8 = 11;
-
-    parachain_asset_location!(EQ, 25_969);
-    parachain_asset_location!(EQD, 6_648_164);
-}
 
 pub mod moonbeam {
     use runtime_common::parachain_asset_location;
@@ -52,18 +40,9 @@ pub mod moonbeam {
     );
 }
 
-pub mod polkadex {
-    use runtime_common::parachain_asset_location;
-    use xcm::latest::{Junction::Parachain, Junctions::X1};
-
-    pub const PARA_ID: u32 = 2040;
-
-    parachain_asset_location!(PDEX, X1(Parachain(PARA_ID)));
-}
-
 #[cfg(test)]
 mod tests {
-	use super::{polkadex, equilibrium, moonbeam};
+	use super::{moonbeam};
 	use xcm::{
 		latest::prelude::{AccountKey20, PalletInstance, Parachain},
 		prelude::GeneralIndex,
@@ -83,40 +62,4 @@ mod tests {
 		assert_eq!(junctions.next(), None);
 	}
 
-	#[test]
-	fn test_glmr() {
-		let glmr_loc = moonbeam::GLMR_location();
-		let mut junctions = glmr_loc.interior().into_iter();
-
-		assert_eq!(junctions.next(), Some(&Parachain(moonbeam::PARA_ID)));
-		assert_eq!(junctions.next(), Some(&PalletInstance(moonbeam::BALANCES_PALLET_INDEX)));
-		assert_eq!(junctions.next(), None);
-	}
-
-    #[test]
-	fn test_pink() {
-		let pink_loc = asset_hub::PINK_location();
-		let mut junctions = pink_loc.interior().into_iter();
-
-		assert_eq!(junctions.next(), Some(&Parachain(asset_hub::PARA_ID)));
-		assert_eq!(junctions.next(), Some(&PalletInstance(asset_hub::ASSET_PALLET_INDEX)));
-		assert_eq!(junctions.next(), Some(&GeneralIndex(asset_hub::PINK_ASSET_ID)));
-		assert_eq!(junctions.next(), None);
-	}
-
-
-	#[test]
-	fn test_constants() {
-		let expected_eq_value = 25_969;
-		assert_eq!(equilibrium::EQ_ASSET_ID, expected_eq_value);
-
-		let eq_interior = equilibrium::EQ_location().interior;
-		let mut junctions = eq_interior.into_iter();
-
-		assert_eq!(junctions.next(), Some(Parachain(equilibrium::PARA_ID)));
-		assert_eq!(junctions.next(), Some(PalletInstance(equilibrium::ASSET_PALLET_INDEX)));
-		assert_eq!(junctions.next(), Some(GeneralIndex(equilibrium::EQ_ASSET_ID)));
-		assert_eq!(junctions.next(), None);
-
-	}
 }

4a7c558bd759ed222f3fc0d9fb41480af399600b
bogdanS98 <bogdan.sandu13@gmail.com>
2024-05-17 13:43:52 +0300

    Bump Foucoco spec version to 16

--- runtime/foucoco/src/lib.rs
+++ runtime/foucoco/src/lib.rs
@@ -277,7 +277,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("foucoco"),
 	impl_name: create_runtime_str!("foucoco"),
 	authoring_version: 1,
-	spec_version: 15,
+	spec_version: 16,
 	impl_version: 0,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 8,

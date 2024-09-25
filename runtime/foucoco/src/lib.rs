4766df8cd0342d9e04c55e0817fcaaec23813187
Marcel Ebert <mail@marcel-ebert.de>
2024-09-25 11:45:11 +0200

    Revert spec version to `19`

--- runtime/foucoco/src/lib.rs
+++ runtime/foucoco/src/lib.rs
@@ -254,7 +254,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("foucoco"),
 	impl_name: create_runtime_str!("foucoco"),
 	authoring_version: 1,
-	spec_version: 20,
+	spec_version: 19,
 	impl_version: 0,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 8,

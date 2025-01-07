75509c0bebe40d1a653e977d57818ae9a9b6031c
Gianfranco <g.tasteri@gmail.com>
2025-01-07 14:03:36 -0300

    bump spec version

--- runtime/amplitude/src/lib.rs
+++ runtime/amplitude/src/lib.rs
@@ -228,7 +228,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("amplitude"),
 	impl_name: create_runtime_str!("amplitude"),
 	authoring_version: 1,
-	spec_version: 19,
+	spec_version: 20,
 	impl_version: 0,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 13,
--- runtime/foucoco/src/lib.rs
+++ runtime/foucoco/src/lib.rs
@@ -230,7 +230,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("foucoco"),
 	impl_name: create_runtime_str!("foucoco"),
 	authoring_version: 1,
-	spec_version: 19,
+	spec_version: 20,
 	impl_version: 0,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 8,
